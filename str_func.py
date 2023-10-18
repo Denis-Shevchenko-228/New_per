51c3fb70712a62ff60f4625190fe2fd1ecc39b28
Apchihba <denis.16032003@gmail.com>
2023-10-18 14:32:15 +0300

    upper str 2.0

--- str_func.py
+++ str_func.py
@@ -1,7 +1,12 @@
 def upper(x):
+    """Функция преобразования строки в верхний регистр"""
     upper_s = []
     for c in x:
         if c.upper():
             upper_s.append(c)
 
     return "".join(upper_s)
+
+def upper_sring(string):
+    """Функция преобразования первой буквы в верхний регистр"""
+    return string.title()
